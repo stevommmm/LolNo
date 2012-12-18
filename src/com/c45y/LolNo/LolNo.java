52fd3034603e3770130a130982b907f406e49696
Stephen Mcgregor <tehborris@gmail.com>
2012-12-18 14:19:26 +1000

    expanded upon original pull req, added config whitelist for unblocked commands while muted

--- src/com/c45y/LolNo/LolNo.java
+++ src/com/c45y/LolNo/LolNo.java
@@ -1,6 +1,7 @@
 package com.c45y.LolNo;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Logger;
 import org.bukkit.ChatColor;
 import org.bukkit.OfflinePlayer;
@@ -9,168 +10,166 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
+
 public class LolNo extends JavaPlugin {
-	// Some toggle variables.
-	public boolean chat_enabled;
-	public boolean command_enabled;
-	public boolean join_enabled;
-	public boolean block_enabled;
-	public ArrayList<String> mutedUsers = new ArrayList<String>(); 
+    // Some toggle variables.
 
-	private final LolNoHandle loglistener = new LolNoHandle(this);
-	Logger log = Logger.getLogger("Minecraft");
+    public boolean chat_enabled;
+    public boolean command_enabled;
+    public boolean join_enabled;
+    public boolean block_enabled;
+    public ArrayList<String> mutedUsers = new ArrayList<String>();
+    public List<String> allowedCommands;
+    private final LolNoHandle loglistener = new LolNoHandle(this);
+    Logger log = Logger.getLogger("Minecraft");
 
-	public void onEnable(){
-		PluginManager pm = getServer().getPluginManager();
-		pm.registerEvents(loglistener, this);
+    @Override
+    public void onEnable() {
+        PluginManager pm = getServer().getPluginManager();
+        pm.registerEvents(loglistener, this);
 
-		getConfig().options().copyDefaults(true);
-		getConfig().addDefault("LolNo.blocks.chat", false);
-		getConfig().addDefault("LolNo.blocks.command", false);
-		getConfig().addDefault("LolNo.blocks.part", false);
-		getConfig().addDefault("LolNo.blocks.block", false);
+        getConfig().options().copyDefaults(true);
+        getConfig().addDefault("LolNo.blocks.chat", false);
+        getConfig().addDefault("LolNo.blocks.command", false);
+        getConfig().addDefault("LolNo.blocks.part", false);
+        getConfig().addDefault("LolNo.blocks.block", false);
+        getConfig().addDefault("muted.allow.commands", new String[]{"modreq", "message", "help", "tell"});
 
-		mutedUsers.addAll(getConfig().getStringList("muted.users"));
+        mutedUsers.addAll(getConfig().getStringList("muted.users"));
 
-		chat_enabled = getConfig().getBoolean("LolNo.blocks.chat");
-		command_enabled = getConfig().getBoolean("LolNo.blocks.command");
-		join_enabled = getConfig().getBoolean("LolNo.blocks.part");
-		block_enabled = getConfig().getBoolean("LolNo.blocks.block");
+        chat_enabled = getConfig().getBoolean("LolNo.blocks.chat");
+        command_enabled = getConfig().getBoolean("LolNo.blocks.command");
+        join_enabled = getConfig().getBoolean("LolNo.blocks.part");
+        block_enabled = getConfig().getBoolean("LolNo.blocks.block");
+        allowedCommands = getConfig().getStringList("muted.allow.commands");
+        saveConfig();
+    }
 
-		saveConfig();
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (sender.hasPermission("LolNo.admin") || sender.isOp()) {
+            if (cmd.getName().equalsIgnoreCase("lolno")) {
+                if (args.length == 1) {
+                    if (args[0].equals("status")) {
+                        printStatus(sender);
+                        return true;
+                    }
+                    if (args[0].equals("help")) {
+                        printHelp(sender);
+                        return true;
+                    }
+                    if (args[0].equals("chat")) {
+                        chat_enabled = !chat_enabled;
+                        toggleConfig("LolNo.blocks.chat");
+                        printStatus(sender);
+                        return true;
+                    }
+                    if (args[0].equals("part")) {
+                        join_enabled = !join_enabled;
+                        toggleConfig("LolNo.blocks.part");
+                        printStatus(sender);
+                        return true;
+                    }
+                    if (args[0].equals("blocks")) {
+                        block_enabled = !block_enabled;
+                        toggleConfig("LolNo.blocks.block");
+                        printStatus(sender);
+                        return true;
+                    }
+                    if (args[0].equals("commands")) {
+                        command_enabled = !command_enabled;
+                        toggleConfig("LolNo.blocks.command");
+                        printStatus(sender);
+                        return true;
+                    }
+                }
+            }
+        }
+        if (sender.hasPermission("lolno.mod") || sender.isOp()) {
+            if (cmd.getName().equalsIgnoreCase("mutedlist")) {
+                sender.sendMessage("Muted users:");
+                for (String player : mutedUsers) {
+                    sender.sendMessage(ChatColor.GRAY + "  - " + player);
+                }
+            }
+            if (cmd.getName().equalsIgnoreCase("mute")) {
+                if (args.length == 1) {
+                    Player mutee = getServer().getPlayer(args[0]);
+                    if (mutee != null) {
+                        if (!mutedUsers.contains(mutee.getName()) && mutee.isOnline()) {
+                            addMuteUser(mutee.getName());
+                            mutee.sendMessage(ChatColor.AQUA + "You have been muted by a member of staff.");
+                            messageStaff(ChatColor.AQUA + sender.getName() + " has muted " + mutee.getName());
+                        }
+                        return true;
+                    }
+                }
+            }
+            if (cmd.getName().equalsIgnoreCase("unmute")) {
+                if (args.length == 1) {
+                    OfflinePlayer mutee = (OfflinePlayer) getServer().getPlayer(args[0]);
+                    if (mutee == null) {
+                        for (OfflinePlayer p : getServer().getOfflinePlayers()) {
+                            if (args[0].toLowerCase().equals(p.getName().toLowerCase())) {
+                                mutee = p;
+                            }
+                        }
+                    }
+                    if (mutee != null) {
+                        removeMuteUser(mutee.getName());
+                        if (mutee instanceof Player) {
+                            ((Player) mutee).sendMessage(ChatColor.AQUA + "You have been unmuted by a member of staff.");
+                        }
+                        messageStaff(ChatColor.AQUA + sender.getName() + " has unmuted " + mutee.getName());
+                    }
+                    return true;
+                }
+            }
+        }
+        return true;
+    }
 
-		log.info("LolNo enabled.");
-	}
+    private void addMuteUser(String player) {
+        mutedUsers.add(player);
+        getConfig().set("muted.users", mutedUsers.toArray());
+        saveConfig();
+    }
 
-	public void onDisable(){
-		log.info("LolNo disabled.");
-	}
-	@Override
-	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		if (sender.hasPermission("LolNo.admin") || sender.isOp()) {
-			if (cmd.getName().equalsIgnoreCase("lolno")) {
-				if (args.length == 1){
-					if(args[0].equals("status")) {
-						printStatus(sender);
-						return true;
-					}
-					if(args[0].equals("help")) {
-						printHelp(sender);
-						return true;
-					}
-					if(args[0].equals("chat")) {
-						chat_enabled = !chat_enabled;
-						toggleConfig("LolNo.blocks.chat");
-						printStatus(sender);
-						return true;
-					}
-					if(args[0].equals("part")) {
-						join_enabled = !join_enabled;
-						toggleConfig("LolNo.blocks.part");
-						printStatus(sender);
-						return true;
-					}
-					if(args[0].equals("blocks")) {
-						block_enabled = !block_enabled;
-						toggleConfig("LolNo.blocks.block");
-						printStatus(sender);
-						return true;
-					}
-					if(args[0].equals("commands")) {
-						command_enabled = !command_enabled;
-						toggleConfig("LolNo.blocks.command");
-						printStatus(sender);
-						return true;
-					}
-				}
-			}
-		}
-		if (sender.hasPermission("lolno.mod") || sender.isOp()) {
-			/* Change command name, this is to stop conflicts right now */
-			if (cmd.getName().equalsIgnoreCase("mutedlist")) {
-				sender.sendMessage("Muted users:");
-				for (String player : mutedUsers) {
-					sender.sendMessage(ChatColor.GRAY + "  - " + player);
-		    	}
-			}
-			if (cmd.getName().equalsIgnoreCase("mute")) {
-				if (args.length == 1) {
-					Player mutee = getServer().getPlayer(args[0]);
-					if(mutee != null) {
-						if(!mutedUsers.contains(mutee.getName()) && mutee.isOnline()) {
-							addMuteUser(mutee.getName());
-							mutee.sendMessage(ChatColor.AQUA + "You have been muted by a member of staff.");
-							messageStaff(ChatColor.AQUA + sender.getName() + " has muted " + mutee.getName());
-						}
-						return true;
-					}
-				}
-			}
-			if (cmd.getName().equalsIgnoreCase("unmute")) {
-				if (args.length == 1){
-					OfflinePlayer mutee = (OfflinePlayer)getServer().getPlayer(args[0]);
-					if(mutee == null) {
-						for(OfflinePlayer p : getServer().getOfflinePlayers()) {
-							if (args[0].toLowerCase().equals(p.getName().toLowerCase())) {
-								mutee = p;
-							}
-						}
-					}
-					if(mutee != null) {
-						removeMuteUser(mutee.getName());
-						if(mutee instanceof Player) {
-							((Player)mutee).sendMessage(ChatColor.AQUA + "You have been unmuted by a member of staff.");
-						}
-						messageStaff(ChatColor.AQUA + sender.getName() + " has unmuted " + mutee.getName());
-					}
-					return true;
-				}
-			}
-		}
-		return true;
-	}
+    private void removeMuteUser(String player) {
+        if (mutedUsers.contains(player.toLowerCase())) {
+            mutedUsers.remove(player.toLowerCase());
+            getConfig().set("muted.users", mutedUsers.toArray());
+        }
+        saveConfig();
+    }
 
-	private void addMuteUser(String player) {
-		mutedUsers.add(player);
-		getConfig().set("muted.users", mutedUsers.toArray());
-		saveConfig();
-	}
+    private void toggleConfig(String node) {
+        getConfig().set(node, !getConfig().getBoolean(node));
+        saveConfig();
+    }
 
-	private void removeMuteUser(String player) {
-		if(mutedUsers.contains(player.toLowerCase())) {
-			mutedUsers.remove(player.toLowerCase());
-			getConfig().set("muted.users", mutedUsers.toArray());
-		}
-		saveConfig();
-	}
+    private void printHelp(CommandSender sender) {
+        sender.sendMessage(ChatColor.GRAY + "Usage: /lolno [param]:");
+        sender.sendMessage(ChatColor.GRAY + "    chat - Disable all chat messages.");
+        sender.sendMessage(ChatColor.GRAY + "    commands - Disable the use of all commands for non staff.");
+        sender.sendMessage(ChatColor.GRAY + "    part - Disable all join/part messages.");
+        sender.sendMessage(ChatColor.GRAY + "    blocks - Disable all block creation/destruction.");
+        sender.sendMessage(ChatColor.GRAY + "    status - Show the current status of lockdown.");
+    }
 
-	private void toggleConfig(String node) {
-		getConfig().set(node, !getConfig().getBoolean(node));
-		saveConfig();
-	}
+    public void printStatus(CommandSender sender) {
+        sender.sendMessage(ChatColor.GRAY + "Lockdown status:");
+        sender.sendMessage(ChatColor.GRAY + "    Commands: " + (command_enabled ? "blocked" : "allowed"));
+        sender.sendMessage(ChatColor.GRAY + "    Chat: " + (chat_enabled ? "blocked" : "allowed"));
+        sender.sendMessage(ChatColor.GRAY + "    Part: " + (join_enabled ? "blocked" : "allowed"));
+        sender.sendMessage(ChatColor.GRAY + "    Blocks: " + (block_enabled ? "blocked" : "allowed"));
+    }
 
-	private void printHelp(CommandSender sender) {
-		sender.sendMessage(ChatColor.GRAY + "Usage: /lolno [param]:");
-		sender.sendMessage(ChatColor.GRAY + "    chat - Disable all chat messages.");
-		sender.sendMessage(ChatColor.GRAY + "    commands - Disable the use of all commands for non staff.");
-		sender.sendMessage(ChatColor.GRAY + "    part - Disable all join/part messages.");
-		sender.sendMessage(ChatColor.GRAY + "    blocks - Disable all block creation/destruction.");
-		sender.sendMessage(ChatColor.GRAY + "    status - Show the current status of lockdown.");
-	}
-
-	public void printStatus(CommandSender sender) {
-		sender.sendMessage(ChatColor.GRAY + "Lockdown status:");
-		sender.sendMessage(ChatColor.GRAY + "    Commands: " + (command_enabled ? "blocked" : "allowed"));
-		sender.sendMessage(ChatColor.GRAY + "    Chat: " + (chat_enabled ? "blocked" : "allowed"));	
-		sender.sendMessage(ChatColor.GRAY + "    Part: " + (join_enabled ? "blocked" : "allowed"));
-		sender.sendMessage(ChatColor.GRAY + "    Blocks: " + (block_enabled ? "blocked" : "allowed"));
-	}
-	
-	public void messageStaff(String message) {
-		for(Player p : getServer().getOnlinePlayers()) {
-			if( p.hasPermission("lolno.mod")) {
-				p.sendMessage(message);
-			}
-		}
-	}
+    public void messageStaff(String message) {
+        for (Player p : getServer().getOnlinePlayers()) {
+            if (p.hasPermission("lolno.mod")) {
+                p.sendMessage(message);
+            }
+        }
+    }
 }
--- src/com/c45y/LolNo/LolNoHandle.java
+++ src/com/c45y/LolNo/LolNoHandle.java
@@ -1,7 +1,6 @@
 package com.c45y.LolNo;
 
 import org.bukkit.ChatColor;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -13,65 +12,67 @@ import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 public class LolNoHandle implements Listener {
-	public final LolNo plugin;
 
-	public LolNoHandle(LolNo instance) {
-		plugin = instance;
-	}
+    public final LolNo plugin;
 
-	@EventHandler(priority = EventPriority.LOWEST , ignoreCancelled = true)
-	public void onCommandPreprocess(PlayerCommandPreprocessEvent event) {
-		if ( plugin.command_enabled ) {
-			if (!event.getPlayer().hasPermission("LolNo.admin") && !event.getPlayer().hasPermission("LolNo.mod")) {
-				plugin.log.info("LolNo blocked: " + event.getMessage() + " from " + event.getPlayer().getName());
-				event.setCancelled(true);
-			}
-		}
-		String[] args = event.getMessage().substring(1).split(" ");
-		if(args[0].equalsIgnoreCase("me")) {
-				Player mutee = event.getPlayer();
-				if(plugin.mutedUsers.contains(mutee.getPlayerListName()))
-					event.setCancelled(true);
-			}
-	}
+    public LolNoHandle(LolNo instance) {
+        plugin = instance;
+    }
 
-	@EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
-	public void onPlayerChat(AsyncPlayerChatEvent event) {
-		if ( plugin.chat_enabled ) {
-			plugin.log.info("LolNo blocked: " + event.getPlayer().getName() + " said: " + event.getMessage());
-			event.setCancelled(true);
-		}
-		if (plugin.mutedUsers.contains((event.getPlayer().getName()))) {
-			event.setCancelled(true);
-			event.getPlayer().sendMessage(ChatColor.DARK_RED + "You are muted, try politely requesting a mod to unmute you");
-		}
-	}
+    @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
+    public void onCommandPreprocess(PlayerCommandPreprocessEvent event) {
+        if (plugin.command_enabled) {
+            if (!event.getPlayer().hasPermission("LolNo.admin") && !event.getPlayer().hasPermission("LolNo.mod")) {
+                plugin.log.info("LolNo blocked: " + event.getMessage() + " from " + event.getPlayer().getName());
+                event.setCancelled(true);
+            }
+        }
+        String[] args = event.getMessage().substring(1).split(" ");
+        if (plugin.mutedUsers.contains(event.getPlayer().getName())) {
+            if (plugin.allowedCommands.contains(args[0].toLowerCase())) {
+                    return;
+            }
+            event.setCancelled(true);
+        }
+    }
 
-	@EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
-	public void onPlayerJoin(PlayerJoinEvent event) {
-		if ( plugin.join_enabled ) {
-			event.setJoinMessage("");
-		}
-	}
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    public void onPlayerChat(AsyncPlayerChatEvent event) {
+        if (plugin.chat_enabled) {
+            plugin.log.info("LolNo blocked: " + event.getPlayer().getName() + " said: " + event.getMessage());
+            event.setCancelled(true);
+        }
+        if (plugin.mutedUsers.contains((event.getPlayer().getName()))) {
+            event.setCancelled(true);
+            event.getPlayer().sendMessage(ChatColor.DARK_RED + "You are muted, try politely requesting a mod to unmute you (/modreq)");
+        }
+    }
 
-	@EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
-	public void onPlayerQuit(PlayerQuitEvent event) {
-		if ( plugin.join_enabled ) {
-			event.setQuitMessage("");
-		}
-	}
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        if (plugin.join_enabled) {
+            event.setJoinMessage("");
+        }
+    }
 
-	@EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
-	public void onBlockBreak(BlockBreakEvent event) {
-		if ( plugin.block_enabled ) {
-			event.setCancelled(true);
-		}
-	}
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    public void onPlayerQuit(PlayerQuitEvent event) {
+        if (plugin.join_enabled) {
+            event.setQuitMessage("");
+        }
+    }
 
-	@EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
-	public void onBlockPlace(BlockPlaceEvent event) {
-		if ( plugin.block_enabled ) {
-			event.setCancelled(true);
-		}
-	}
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    public void onBlockBreak(BlockBreakEvent event) {
+        if (plugin.block_enabled) {
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    public void onBlockPlace(BlockPlaceEvent event) {
+        if (plugin.block_enabled) {
+            event.setCancelled(true);
+        }
+    }
 }
